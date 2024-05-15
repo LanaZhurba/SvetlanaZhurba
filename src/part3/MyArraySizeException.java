c0f58fa9e0d8ecda3ad90daffeb91624244ae8b4
SvetlanaZhurba <SvetlanaZhurba>
2024-05-15 17:33:09 +0300

    My exception

---
+++ src/part3/MyArraySizeException.java
@@ -0,0 +1,11 @@
+package part3;
+
+public class MyArraySizeException extends Exception{
+    public MyArraySizeException () {
+        super();
+    }
+
+    public MyArraySizeException (String message) {
+        super(message);
+    }
+}
