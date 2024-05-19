69b84317eec2d9226d458b8ffa71cedd2faa02e2
SvetlanaZhurba <SvetlanaZhurba>
2024-05-19 21:40:47 +0300

    add new class and delete

--- src/part3/main.java
+++ src/part3/main.java
@@ -4,27 +4,13 @@ import java.sql.Array;
 
 public class main {
     public static void main(String[] args) throws MyArraySizeException{
-        String[][] array = {{"3", "2", "2", "1"}, {"ь", "2", "2", "1"}, {"3", "2", "2", "1"}, {"3", "2", "2", "1"}};
+        String[][] array = {{"3", "2", "2", "1"}, {"ь", "2", "2", "1"}, { "2", "2", "1"}, {"3", "2", "2", "1"}};
+        CalculateSumOfArray calculateSumOfArray = new CalculateSumOfArray();
+
         try {
-            System.out.println("Сумма элементов в массиве = " + getSumOfArray(array));
+            System.out.println("Сумма элементов в массиве = " + calculateSumOfArray.getSumOfArray(array));
         } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    public static int getSumOfArray(String[][] arr) throws MyArrayDataException, MyArraySizeException {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr.length != 4 || arr[i].length != 4) {
-                    throw new MyArraySizeException("Введенный массив не размером 4х4");}
-                try {
-                    sum += Integer.parseInt(arr[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("В ячейке элемент ["+ i + "," + j + "] некорректный");
-                }
-            }
-        }
-        return sum;
     }
 }
