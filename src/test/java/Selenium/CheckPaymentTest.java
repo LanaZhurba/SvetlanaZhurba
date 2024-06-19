41b9c61b0adf5467915c171308b0825fae80480c
Svetlyaka <seatv2010@mail.ru>
2024-06-19 13:15:54 +0300

    part2

--- src/test/java/Selenium/CheckPaymentTest.java
+++ src/test/java/Selenium/CheckPaymentTest.java
@@ -48,7 +48,6 @@ public class CheckPaymentTest {
 //    @Test
 //    @DisplayName("Проверка перехода по ссылке")
 //    public void testLink(){
-//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 //        driver.findElement(By.xpath("//div[@class='pay__wrapper']/a")).click();
 //        Assertions.assertTrue(driver.findElement(By.xpath("//span[text()='Порядок оплаты и безопасность интернет платежей']")).isDisplayed(),"нет перехода на другую вкладку");
 //        driver.get("https://www.mts.by/");
@@ -61,16 +60,10 @@ public class CheckPaymentTest {
 //        driver.findElement(By.xpath("//p[text()='Услуги связи']")).click();
 //        driver.findElement(By.xpath("//input[@class='phone']")).sendKeys("297777777");
 //        driver.findElement(By.xpath("//input[@id='connection-sum']")).sendKeys("100");
-//        driver.findElement(By.xpath("//form[@id='pay-connection']/button[@type='submit']")).click();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='bepaid-iframe']")));
-//        boolean isDisp = driver.findElement(By.xpath("//*[contains(text(), 'Оплата:')]")).isDisplayed();
-//        if (isDisp) {
-//            String actual = driver.findElement(By.xpath("//*[contains(text(), 'Оплата:')]")).getText();
-//            Assertions.assertEquals(actual, "Оплата: Услуги связи Номер:375297777777", "Не прогрузилось окно оплаты");
-//        }
+//        driver.findElement(By.xpath("//form[@id='pay-connection']/button")).click();
+//        driver.switchTo().frame(wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@class='bepaid-iframe']"))));
+//        Assertions.assertEquals(driver.findElement(By.xpath("//span[@class='pay-description__text']")).getText(), "Оплата: Услуги связи Номер:375297777777");
 //    }
-
 
     private static void acceptCookies() {
         try {
@@ -88,11 +81,11 @@ public class CheckPaymentTest {
     public void checkFirstField(){
         driver.findElement(By.xpath("//button[@class='select__header']")).click();
         driver.findElement(By.xpath("//p[text()='Услуги связи']")).click();
-        String popupName = driver.findElement(By.xpath("//p[text()='Услуги связи']")).getText();
+        String popupName = driver.findElement(By.xpath("//button/span[@class='select__now']")).getText();
         Assertions.assertEquals("Услуги связи",popupName);
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Номер телефона']")).getAttribute("placeholder"),"Номер телефона");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Сумма']")).getAttribute("placeholder"),"Сумма");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@id='connection-email']")).getAttribute("placeholder"),"E-mail для отправки чека");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-connection']//input[@placeholder='Номер телефона']")).isEnabled(),"Не отображается подсказка Номер телефона");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-connection']//input[@placeholder='Сумма']")).isEnabled(),"Не отображается подсказка Сумма");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-connection']//input[@placeholder='E-mail для отправки чека']")).isEnabled(),"Не отображается подсказка E-mail для отправки чека");
     }
 
     @Test
@@ -100,11 +93,11 @@ public class CheckPaymentTest {
     public void checkSecondField(){
         driver.findElement(By.xpath("//button[@class='select__header']")).click();
         driver.findElement(By.xpath("//p[text()='Домашний интернет']")).click();
-        String popupName = driver.findElement(By.xpath("//p[text()='Домашний интернет']")).getText();
+        String popupName = driver.findElement(By.xpath("//button/span[@class='select__now']")).getText();
         Assertions.assertEquals("Домашний интернет",popupName);
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Номер абонента']")).getAttribute("placeholder"),"Номер абонента");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Сумма']")).getAttribute("placeholder"),"Сумма");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@id='connection-email']")).getAttribute("placeholder"),"E-mail для отправки чека");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-internet']//input[@placeholder='Номер абонента']")).isEnabled(),"Не отображается подсказка Номер абонента");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-internet']//input[@placeholder='Сумма']")).isEnabled(), "Не отображается подсказка Сумма");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-internet']//input[@placeholder='E-mail для отправки чека']")).isEnabled(),"Не отображается подсказка E-mail для отправки чека");
     }
 
     @Test
@@ -112,11 +105,11 @@ public class CheckPaymentTest {
     public void checkThirdField(){
         driver.findElement(By.xpath("//button[@class='select__header']")).click();
         driver.findElement(By.xpath("//p[text()='Рассрочка']")).click();
-        String popupName = driver.findElement(By.xpath("//p[text()='Рассрочка']")).getText();
+        String popupName = driver.findElement(By.xpath("//button/span[@class='select__now']")).getText();
         Assertions.assertEquals("Рассрочка",popupName);
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Номер счета на 44']")).getAttribute("placeholder"),"Номер счета на 44");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Сумма']")).getAttribute("placeholder"),"Сумма");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@id='connection-email']")).getAttribute("placeholder"),"E-mail для отправки чека");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-instalment']//input[@placeholder='Номер счета на 44']")).isEnabled(),"Не отображается подсказка Сумма");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-instalment']//input[@placeholder='Сумма']")).isEnabled(), "Не отображается подсказка Сумма");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-instalment']//input[@placeholder='E-mail для отправки чека']")).isEnabled(),"Не отображается подсказка E-mail для отправки чека");
     }
 
     @Test
@@ -124,11 +117,11 @@ public class CheckPaymentTest {
     public void checkLastField(){
         driver.findElement(By.xpath("//button[@class='select__header']")).click();
         driver.findElement(By.xpath("//p[text()='Задолженность']")).click();
-        String popupName = driver.findElement(By.xpath("//p[text()='Задолженность']")).getText();
+        String popupName = driver.findElement(By.xpath("//button/span[@class='select__now']")).getText();
         Assertions.assertEquals("Задолженность",popupName);
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Номер счета на 2073']")).getAttribute("placeholder"),"Номер счета на 2073");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@placeholder='Сумма']")).getAttribute("placeholder"),"Сумма");
-        Assertions.assertEquals(driver.findElement(By.xpath("//input[@id='connection-email']")).getAttribute("placeholder"),"E-mail для отправки чека");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-arrears']//input[@placeholder='Номер счета на 2073']")).isEnabled(), "Не отображается подсказка Номер счета на 2073");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-arrears']//input[@placeholder='Сумма']")).isEnabled(),"Не отображается подсказка Сумма");
+        Assertions.assertTrue(driver.findElement(By.xpath("//form[@id='pay-arrears']//input[@placeholder='E-mail для отправки чека']")).isEnabled(),"Не отображается подсказка E-mail для отправки чека");
     }
 
     @Test
@@ -140,11 +133,9 @@ public class CheckPaymentTest {
         driver.findElement(By.xpath("//input[@id='connection-sum']")).sendKeys("100");
         driver.findElement(By.xpath("//input[@id='connection-email']")).sendKeys("see@mail.ru");
         driver.findElement(By.xpath("//form[@id='pay-connection']/button[@type='submit']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 30);
         driver.switchTo().frame(wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@class='bepaid-iframe']"))));
         String sumInButtonText = driver.findElement(By.xpath("//button[@type='submit']")).getAttribute("innerHTML");
         String actualValue = driver.findElement(By.xpath("//*[contains(text(), 'Оплата:')]")).getAttribute("innerHTML");
-
         Assertions.assertEquals("100.00 BYN",driver.findElement(By.xpath("//div[@class='pay-description__cost']//span[1]")).getAttribute("innerHTML"));
         Assertions.assertEquals(" Оплатить  100.00 BYN <!---->", sumInButtonText);
         Assertions.assertEquals(actualValue, "Оплата: Услуги связи\n" +
@@ -153,10 +144,10 @@ public class CheckPaymentTest {
         Assertions.assertEquals("Срок действия", driver.findElement(By.xpath("//label[@class='ng-tns-c46-4 ng-star-inserted']")).getAttribute("innerHTML"));
         Assertions.assertEquals("Имя держателя (как на карте)", driver.findElement(By.xpath("//label[@class='ng-tns-c46-3 ng-star-inserted']")).getAttribute("innerHTML"));
         Assertions.assertEquals("CVC", driver.findElement(By.xpath("//label[@class='ng-tns-c46-5 ng-star-inserted']")).getAttribute("innerHTML"));
-        Assertions.assertTrue(driver.findElement(By.xpath("//img[@src='assets/images/payment-icons/card-types/mastercard-system.svg']")).isDisplayed(), "Иконка MasterCard не отображается");
-        Assertions.assertTrue(driver.findElement(By.xpath("//img[@src='assets/images/payment-icons/card-types/visa-system.svg']")).isDisplayed(), "Иконка Visa не отображается");
-        Assertions.assertTrue(driver.findElement(By.xpath("//img[@src='assets/images/payment-icons/card-types/belkart-system.svg']")).isDisplayed(), "Иконка Белкард не отображается");
-        Assertions.assertTrue(driver.findElement(By.xpath("//div[@class='cards-brands cards-brands_random ng-tns-c61-0 ng-star-inserted']")).isDisplayed(), "Иконка Мир не отображается");
+        Assertions.assertTrue(driver.findElement(By.xpath("//img[@src='assets/images/payment-icons/card-types/mastercard-system.svg']")).isEnabled(), "Иконка MasterCard не отображается");
+        Assertions.assertTrue(driver.findElement(By.xpath("//img[@src='assets/images/payment-icons/card-types/visa-system.svg']")).isEnabled(), "Иконка Visa не отображается");
+        Assertions.assertTrue(driver.findElement(By.xpath("//img[@src='assets/images/payment-icons/card-types/belkart-system.svg']")).isEnabled(), "Иконка Белкард не отображается");
+        Assertions.assertTrue(driver.findElement(By.xpath("//div[@class='cards-brands cards-brands_random ng-tns-c61-0 ng-star-inserted']")).isEnabled(), "Иконка Мир не отображается");
         driver.get("https://www.mts.by/");
     }
 
